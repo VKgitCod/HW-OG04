b20f7a90addb2a4b3c6b3febacab575a2c2d0c9b
VKgitCod <itcreatorvak@gmail.com>
2024-07-09 21:27:13 +0300

    test

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-a = float(input("Введи число:"))
-
-if a > 10:
-    print(f"Число {a} больше 10")
-elif a < 10:
-    print(f"Число {a} меньше 10")
-else:
-    print(f"Число {a} = 10")
+a = int(input("Как вас зовут?"))
