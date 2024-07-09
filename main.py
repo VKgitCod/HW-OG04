3d49b52216dd2342852c42356b5a7f38a04fd5ed
VKgitCod <itcreatorvak@gmail.com>
2024-07-09 21:27:52 +0300

    Revert "test"
    This reverts commit b20f7a90addb2a4b3c6b3febacab575a2c2d0c9b.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-a = int(input("Как вас зовут?"))
+a = float(input("Введи число:"))
+
+if a > 10:
+    print(f"Число {a} больше 10")
+elif a < 10:
+    print(f"Число {a} меньше 10")
+else:
+    print(f"Число {a} = 10")
