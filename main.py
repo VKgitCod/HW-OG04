7af90a4c64d8d9657b5b72ae712ad126c25a62a4
VKgitCod <itcreatorvak@gmail.com>
2024-07-09 21:33:56 +0300

    add new code wood

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-a = float(input("Введи число:"))
+a = str("Лес")
 
-if a > 10:
-    print(f"Число {a} больше 10")
-elif a < 10:
-    print(f"Число {a} меньше 10")
-else:
-    print(f"Число {a} = 10")
